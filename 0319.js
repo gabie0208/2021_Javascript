02306c54f9a9118c711491e6868eb6548c65c2b4
Ko Hyejung <79845947+gabie0208@users.noreply.github.com>
2021-03-20 00:12:32 +0900

    Add files via upload

---
+++ 0319.js
@@ -0,0 +1,14 @@
+function change1() {
+    var mo = document.querySelector('.mo');
+    mo.innerHTML = '모이지말고'
+}
+
+function change2() {
+    var gak = document.querySelector('.gak');
+    gak.innerHTML = '각자 집에서'
+}
+
+function change3() {
+    var ko = document.querySelector('.ko');
+    ko.innerHTML = '코딩 공부합시다!'
+}
